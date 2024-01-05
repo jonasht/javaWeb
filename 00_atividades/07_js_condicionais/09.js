40ee956218166b208654ec2db8390168f2942b78
jonasht <jhenriquet@outlook.com.br>
2024-01-05 09:56:57 -0300

    arrumando as dirs de atividades

--- 00_atividades/07_js_condicionais/09.js
+++ 00_atividades/07_js_condicionais/09.js
@@ -3,19 +3,31 @@
 //  o jogador tem que adivinhar qual é.
 //  O programa deve fornecer dicas se o número fornecido é maior ou menor.
 
-// coloque um numero aleatorio de 0 a 10
-let palpite = 10
-// gerar numero aleatorio
-let numeroAleatorio = Math.floor(Math.random() * 10) + 1; // Gera um número aleatório de 1 a 100
-    
+
+function jogoAdivinhacao() {
+    const numeroAleatorio = Math.floor(Math.random() * 100) + 1; 
+    let tentativas = 0;
   
-if (palpite === numeroAleatorio) {
-    console.log(`Parabéns! Você acertou`)
-}
-
-
-
-
+    while (true) {
+      const palpite = parseInt(prompt("Adivinhe o número (de 1 a 100):"));
   
+      if (isNaN(palpite)) {
+        alert("Por favor, insira um número válido.");
+        continue;
+      }
   
+      tentativas++;
+  
+      if (palpite === numeroAleatorio) {
+        alert(`Parabéns! Você acertou em ${tentativas} tentativas.`);
+        break;
+      } else if (palpite < numeroAleatorio) {
+        alert("Tente um número maior.");
+      } else {
+        alert("Tente um número menor.");
+      }
+    }
+  }
+  
+  jogoAdivinhacao();
   
