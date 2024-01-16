d88f89fc97ddf535b217a39d874d84fcb5bceece
jonasht <jhenriquet@outlook.com.br>
2024-01-15 21:38:26 -0300

    atividade terminada

--- 00_atividades/java_03_aula04Java/e1_3.java
+++ 00_atividades/java_03_aula04Java/e1_3.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class e1_3 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] numeros = {8, 9, 2, 4, 5, 41, 100, 25, 47, 63};
+        int numeroAEncontrar = 100;
+
+        if (findNumber(numeros, numeroAEncontrar)) {
+            System.out.println("numero encontrado");
+        } else {
+            System.out.println("numero nao encontrado");
+        }
+    }
+
+    public static boolean findNumber(int[] numeros, int numeroAEncontrar) {
+        Arrays.sort(numeros);
+        int indice = Arrays.binarySearch(numeros, numeroAEncontrar);
+
+        return indice >= 0;
     }
 }
