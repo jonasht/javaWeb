e9a99af12c25294da75a7262722bd7019dae42d4
jonasht <jhenriquet@outlook.com.br>
2024-01-18 12:58:32 -0300

    aula

---
+++ spring/18_jan_24/src/test/java/com/soulcode/soulib/SoulibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcode.soulib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
