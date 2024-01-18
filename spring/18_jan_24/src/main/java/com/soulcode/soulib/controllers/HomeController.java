56e256dba64c77b54b3dc45fad44e467d007d1d5
jonasht <jhenriquet@outlook.com.br>
2024-01-18 13:51:44 -0300

    aula

---
+++ spring/18_jan_24/src/main/java/com/soulcode/soulib/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.soulcode.soulib.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    // ao acessar localhost:8080/home
+    // deve exibir a pagina index
+
+    // configura o metado para responder a uma erquisição
+    // para /home
+    
+    @GetMapping("/home")
+    public String paginaHome(){
+        return "index"; // indica qual view serah exibida 
+    }
+}
